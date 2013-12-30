a54e999ec475a14f25650470fa1d55f520cf4c6b
Grant Palin <grant@grantpalin.com>
2013-12-29 18:24:55 -0800

    Added page header block

--- home.php
+++ home.php
@@ -12,6 +12,14 @@ get_header();
     <div id="primary" class="content-area">
         <div id="content" class="site-content" role="main">
 <?php inet_breadcrumb(); ?>
+<header class="entry-header">
+    <div class="standardsub-left">
+        <img src="<?php bloginfo('template_url'); ?>/img/header-icon-notes.png" />
+    </div>
+    <div class="standardsub-right" style="background:url(<?php bloginfo('template_url'); ?>/img/header-image-blog.jpg) top left no-repeat;">
+        <h1 class="entry-title">infectionNet Blog</h1>
+    </div>
+</header><!-- .entry-header -->
 <?php
 if (have_posts() ):
     inet_content_nav( 'nav-above' );
