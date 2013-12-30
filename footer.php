c161a1e397349ac03a3f9a51e2b8d943b8c50f37
Grant Palin <grant@grantpalin.com>
2013-12-29 17:24:16 -0800

    Simplified markup

--- footer.php
+++ footer.php
@@ -12,35 +12,30 @@
 
             <footer id="colophon" class="site-footer" role="contentinfo">
                 <div class="site-info">
-                    <div class="left">
-                        <p class="infectionnet"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-infectionnet.png" width="245" height="89" alt="infectionNet" /></p><!--
-                        --><p class="viha"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-viha.png" width="160" height="89" alt="Vancouver Island Health Authority" /></p>
-                    </div><!--
-                    --><div class="right">
-                        <nav>
-                            <h1 class="assistive-text">Menu</h1>
-                            <ul>
-                                <li><a href="about.html">About</a></li><!--
-                                --><li><a href="blog.html">The Blog</a></li><!--
-                                --><li><a href="resources.html">Resources</a></li><!--
-                                --><li><a href="cases.html">Cases</a></li><!--
-                                --><li><a href="groups.html">Groups</a></li><!--
-                                --><li><a href="questions.html">Questions</a></li><!--
-                                --><li><a href="contact.html">Contact</a></li>
-                            </ul>
-                        </nav><!--
-                        --><div class="news">
-                            <p class="twitter"><a href="#">Follow infectionNet on Twitter</a></p><!--
-                            --><p class="newsletter"><a href="#">Sign up for our newsletter</a></p>
-                        </div><!--
-                        --><div class="closing">
-                            <p class="copyright">Copyright 2013 Vancouver Island Health Authority, infectionNet</p><!--
-                            --><p class="armada">Site by <a href="http://armadadesign.ca/">Armada Design</a></p>
-                        </div>
-                    </div>
+                    <div class="meta">
+<?php wp_nav_menu( array( 'theme_location' => 'footer', 'container' => 'div' ) ); ?>
+                        <p class="copyright">Copyright 2013 Vancouver Island Health Authority, infectionNet</p>
+                        <p class="armada">Site by <a href="http://armadadesign.ca/">Armada Design</a></p>
+                        <p class="twitter"><a href="#">Follow infectionNet on Twitter</a></p>
+                        <p class="newsletter"><a href="#">Sign up for our newsletter</a></p>
+                    </div><!-- .meta -->
+                    <div class="logos">
+                        <p class="infectionnet"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-infectionnet.png" width="245" height="89" alt="infectionNet" /></p>
+                        <p class="viha"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-viha.png" width="157" height="83" alt="Vancouver Island Health Authority" /></p>
+                    </div><!-- .logos -->
                 </div><!-- .site-info -->
             </footer><!-- #colophon .site-footer -->
         </div><!-- #page .hfeed .site -->
+<script>
+jQuery(document).ready(function($) {
+	// add conditional classname based on support for details / summary
+	$('html').addClass($.fn.details.support ? 'details' : 'no-details');
+	// emulate <details> where necessary and enable open/close event handlers
+	$('details').details();
+	
+	$(document).foundation();
+});
+</script>
 <?php wp_footer(); ?>
     </body>
 </html>
