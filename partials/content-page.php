50633ced2f7979bf3a473218191b854f0ca17c9c
Grant Palin <grant@grantpalin.com>
2013-12-29 18:32:13 -0800

    Added to the page header block

--- partials/content-page.php
+++ partials/content-page.php
@@ -8,13 +8,17 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <div class="standardsub-left">
+            <img src="<?php bloginfo('template_url'); ?>/img/headerimage-icon-about.png" />
+        </div>
+        <div class="standardsub-right" style="background-image:url(<?php bloginfo('template_url'); ?>/img/headerimage-about.jpg) top left no-repeat;">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </div>
     </header><!-- .entry-header -->
 
     <div class="entry-content">
 <?php
 the_content();
-wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'inet' ), 'after' => '</div>' ) );
 edit_post_link( __( 'Edit', 'inet' ), '<span class="edit-link">', '</span>' );
 ?>
     </div><!-- .entry-content -->
