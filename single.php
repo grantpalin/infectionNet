e2f2325781fecb3c62a7bd5624a1b6bdc3cd2d25
Grant Palin <grant@grantpalin.com>
2013-12-29 18:36:53 -0800

    Revised logic, used partials

--- single.php
+++ single.php
@@ -10,18 +10,21 @@ get_header();
 
     <div id="primary" class="content-area">
         <div id="content" class="site-content" role="main">
-<?php inet_breadcrumb(); ?>
 <?php
-while ( have_posts() ) :
-    the_post();
-    inet_content_nav( 'nav-above' );
-    get_template_part( 'partials/content', 'single' );
-    inet_content_nav( 'nav-below' );
+inet_breadcrumb();
 
-    // If comments are open or there is at least one comment, load up the comment template
-    if ( comments_open() || '0' != get_comments_number() )
-        comments_template( '', true );
-endwhile;
+if (have_posts() ):
+    inet_content_nav( 'nav-above' );
+
+    while ( have_posts() ) :
+        the_post();
+        get_template_part( 'partials/content', 'blogsingle' );
+    endwhile;
+
+    inet_content_nav( 'nav-below' );
+else :
+    get_template_part( 'partials/no-results', 'index' );
+endif; // have_posts()
 ?>
         </div><!-- #content .site-content -->
     </div><!-- #primary .content-area -->
