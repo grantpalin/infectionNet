88b6f67401e00201e3c971fff2d3375e1ca3ac73
Grant Palin <grant@grantpalin.com>
2014-01-03 15:36:12 -0800

    Added archive templates for the custom types, along with the matching singular templates.

---
+++ archive-question.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The template for displaying the Questions & Answers archive. Overrides the generic archive.php.
+ *
+ * @package infectionNet
+ * @since infectionNet 0.1
+ */
+get_header();
+?>
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+<?php inet_breadcrumb(); ?>
+<?php if ( have_posts() ) : ?>
+			<header class="archive-header">
+            	<div class="standardsub-left"><img src="<?php bloginfo('template_url'); ?>/img/header-icon-questions.png" /></div>
+                <div class="standardsub-right" style="background:url(<?php bloginfo('template_url'); ?>/img/header-image-clinical-resources.jpg) top left no-repeat;">
+					<h1 class="archive-title"><?php _e( 'Questions & Answers', 'inet' ); ?></h1>
+                </div>
+			</header><!-- .archive-header -->
+
+            <div class="entry-content">
+<?php
+if (!is_paged()):
+	$description = get_post_type_object(get_post_type())->description;
+
+	if ($description):
+?>
+<p class="intro"><?php echo $description; ?></p>
+<?php
+	endif;
+?>
+	            <!--h2>Find Questions</h2>
+
+				<div class="taxonomies">
+<?php
+$taxonomies = array (
+	'infection_types' => 'Infection Type',
+	'micro_organisms' => 'Organism Type',
+	'antibiotic_types' => 'Antibiotic Type'
+);
+
+foreach ($taxonomies as $tax => $title): ?>
+                    <div class="taxonomy">
+                        <div class="inner">
+                            <h3 class="taxonomy-name"><?php echo $title; ?></h3>
+                            <ul class="taxonomy-term-list">
+<?php echo get_post_type_terms($tax); ?>
+                            </ul>
+                        </div>
+                    </div>
+<?php endforeach; ?>
+                </div--> <!-- .taxonomies -->
+			<?php
+				while ( have_posts() ) : the_post();
+            ?>
+                <div id="post-<?php the_ID()?>" <?php post_class('entry-brief'); ?>>
+                    <h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+                    <div class="excerpt"><?php the_excerpt(); ?></div>
+                </div><!--#post-<?php the_ID(); ?>-->
+            <?php
+				endwhile;
+			else: // not on the archive main page, but a later page
+				while ( have_posts() ) : the_post();
+            ?>
+                <div id="post-<?php the_ID()?>" <?php post_class('entry-brief'); ?>>
+                    <h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+                    <div class="excerpt"><?php the_excerpt(); ?></div>
+                </div><!--#post-<?php the_ID(); ?>-->
+            <?php
+				endwhile;
+			endif; // is_paged
+
+			inet_content_nav( 'nav-below' );
+         else :
+			get_template_part( 'content', 'none' );
+		endif; // have_posts
+        ?>
+            </div><!--.entry-content-->
+		</div><!-- #content -->
+	</section><!-- #primary .site-content -->
+<?php get_footer(); ?>
---
+++ single-note.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying single Note entries.
+ *
+ * @package infectionNet
+ * @since infectionNet 0.1
+ */
+get_header();
+?>
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+<?php inet_breadcrumb(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<header class="entry-header">
+                    <div class="standardsub-left"><img src="<?php bloginfo('template_url'); ?>/img/header-icon-notes.png" /></div>
+                    <div class="standardsub-right" style="background:url(<?php bloginfo('template_url'); ?>/img/header-image-clinical-resources.jpg) top left no-repeat;">
+                        <h1 class="entry-title"><?php the_title(); ?></h1>
+                    </div>
+				</header>
+
+                <div class="entry-content">
+                    <div class="entry-content-right">
+                        <?php the_content(); ?>
+                    </div><!--.entry-content-right-->
+                    <div class="entry-content-left">
+                        <div class="taxonomies">
+	                        <h2>Find Similar Content</h2>
+							<?php inet_tax_terms_singular(); ?>
+                        </div><!-- .taxonomies -->
+                        <div class="related-content">
+        	                <?php get_template_part('partials/related'); ?>
+                        </div><!-- .related-content -->
+                    </div><!--.entry-content-left-->
+                </div><!-- .entry-content -->
+
+				<footer class="entry-meta">
+                    <?php edit_post_link( __( 'Edit', 'inet' ), '<p class="edit-link">', '</p>' ); ?>
+				</footer><!-- .entry-meta -->
+			<?php endwhile; // end of the loop. ?>
+			</article><!-- #post -->
+		</div><!-- #content -->
+	</div><!-- #primary -->
+<?php
+get_footer();
+?>
