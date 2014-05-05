cf572a0a5fceb08b5c59e75bbf4745f8f30998bb
Grant Palin <grant@grantpalin.com>
2014-05-04 22:43:38 -0700

    Removed labelling of type of current content

--- partials/content.php
+++ partials/content.php
@@ -9,7 +9,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
         <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'inet' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-        <p><?php echo get_post_type_object(get_post_type())->labels->singular_name; ?></p>
 
         <?php if ( 'post' == get_post_type() ) : // only display post date and author if this is a Post, not a Page. ?>
             <div class="entry-meta">
